6eef470fe49bd60c7f0bd3a87e730d954e202708
Qelemnaz90 <enazy1990@gmail.com>
2024-04-25 23:59:31 +0400

    index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const add_btn = document.querySelectorAll('.add')
 const del_btn = document.querySelectorAll('.del')
 const sum_price = document.querySelector('.price')
 
+
 let obj = {}
 let sum = 0
 
@@ -42,13 +43,11 @@ del_btn.forEach((b) => {
         const name = card_body.querySelector('.card-text').textContent
         removeItem(name)
         sum_price.innerText = sum
-        totalPrice()
+        
     })
 })
 
-function totalPrice(a,b){
-    console.log(a*b);
-}
+
 
 
 function removeItem(name){
@@ -66,12 +65,16 @@ function removeItem(name){
 
 let basket = document.querySelector('.basket')
 let h2 = document.querySelector('.m-5')
-
+let cards = document.querySelector('.cards')
 
 basket.addEventListener('click', function(){
     h2.style.display = "block"
-    let cards = document.querySelector('.cards')
+    let cards2 = document.querySelector('.cards2')
+    cards2.classList.add("d-none")
+    next.style.display = 'none'
     cards.classList.add("d-none")
+
+
     addToBasket();
 })
 
@@ -79,10 +82,15 @@ basket.addEventListener('click', function(){
 
 function addToBasket() {
     h2.innerHTML = ""
+    let h3 = document.createElement("h3")
+    h3.innerText = "Your Order:"
+    h2.appendChild(h3)
     let total = 0
         for (let itemName in obj) {
         const item = obj[itemName];
         const itemInfo = document.createElement('div')
+
+
         itemInfo.innerText=
         `${itemName}  
         Quantity: ${item.quantity}  
@@ -90,11 +98,11 @@ function addToBasket() {
 
 
         itemInfo.style.color= 'violet'
-        itemInfo.style.marginBottom = '10px'
+        itemInfo.style.marginBottom = '20px'
         h2.appendChild(itemInfo)
         
         total += item.quantity * item.price
-    
+        
 
     } const totalElement = document.createElement('div');
     totalElement.innerText = `Total Price: $${total.toFixed(2)}`;
@@ -102,4 +110,36 @@ function addToBasket() {
     h2.appendChild(totalElement);
     console.log("Total Price:", total.toFixed(2))
 }
+
+
+
+
+let next = document.querySelector('.next')
+let previous = document.querySelector('.previous')
+let head = document.querySelector('.head')
+let cards2 = document.querySelector('.cards2');
+// let cards = document.querySelector('.cards')
+
+next.addEventListener('click', function(){
+    cards.classList.add('d-none')
+    cards2.classList.remove('d-none')
+    previous.style.display = 'block'
+    next.style.display = 'none'
+    
+    
+
+})
+
+previous.addEventListener('click', function(){
+    cards2.classList.add('d-none')    
+    previous.style.display = 'none'
+    head.style.display = 'block'
+    cards.classList.remove('d-none')
+    next.style.display = 'block'
+
+})
+
+
+
+
 
